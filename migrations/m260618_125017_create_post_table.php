c2a3a64be39fae40d1a2b88ca4defeef183b49dc
Bhaskar <bhaskar@Bhaskars-MacBook-Air.local>
2026-06-18 18:24:58 +0530

    post table

---
+++ migrations/m260618_125017_create_post_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%post}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%user}}`
+ */
+class m260618_125017_create_post_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'title' => $this->string(255)->notNull(),
+            'body' => $this->text(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+
+        // creates index for column `user_id`
+        $this->createIndex(
+            '{{%idx-post-user_id}}',
+            '{{%post}}',
+            'user_id'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            '{{%fk-post-user_id}}',
+            '{{%post}}',
+            'user_id',
+            '{{%user}}',
+            'id',
+            'CASCADE', // ON DELETE: delete posts when user deleted
+            'RESTRICT' // ON UPDATE
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%user}}`
+        $this->dropForeignKey(
+            '{{%fk-post-user_id}}',
+            '{{%post}}'
+        );
+
+        // drops index for column `user_id`
+        $this->dropIndex(
+            '{{%idx-post-user_id}}',
+            '{{%post}}'
+        );
+
+        $this->dropTable('{{%post}}');
+    }
+}
